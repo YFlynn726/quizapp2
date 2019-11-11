547a83330ddf8eb086aa8ba865a86ead3a8d8068
Yajaira Flynn-Corbin <yary.ba26@gmail.com>
2019-11-11 10:49:53 -0500

    added questions in js and napkin design image

---
+++ script.js
@@ -0,0 +1,52 @@
+//question database
+const STORE = [{
+        question: 'What is the count for dancing salsa On2?',
+        answers: [
+            '123-567',
+            '1234-5678',
+            'No count- just dance to the beat',
+            'Ust stand there and watch everyone else'
+        ],
+        correctAnswer: '123-567'
+    },
+    {
+        question: 'What foot do men start with?',
+        answers: [
+            'Men start with their right foot',
+            'They do not start with their foot at all',
+            'Men hop forward towards their dance partner',
+            'Men start with their left foot'
+        ],
+        correctAnswer: 'Men start with their left foot'
+    },
+    {
+        question: 'Is it ok for men to use their hips?',
+        answers: [
+            'NO, they must dance in a stiff manner',
+            'Yes, let the rythm take over',
+            'Men must move their hips more than their partner',
+            'None of the above'
+        ],
+        correctAnswer: 'Yes, let the rythm take over'
+    },
+    {
+        question: 'What happened to "4" and "8" in the dance count?',
+        answers: [
+            'They do not exist',
+            'The person who created can not count',
+            'The count exist, it is in the air shifting back and forward',
+            'None of the above'
+        ],
+        correctAnswer: 'The count exist, it is in the air shifting back and forward'
+    },
+    {
+        question: 'Do you need to bring a dance partner to class?',
+        answers: [
+            'Yes or you will be dancing solo',
+            'You can dance with the wall',
+            'No need to bring a partner',
+            'None of the above'
+        ],
+        correctAnswer: 'No need to bring a partner'
+    }
+];
